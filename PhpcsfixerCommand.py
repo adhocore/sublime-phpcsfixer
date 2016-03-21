2437cd68d6d755d4206c6eb4597629270639714d
Jitendra Adhikari <jiten.adhikary@gmail.com>
2016-03-21 18:40:49 +0700

    trigger fixer on save

--- PhpcsfixerCommand.py
+++ PhpcsfixerCommand.py
@@ -3,39 +3,43 @@ import sublime
 import subprocess
 import sublime_plugin
 
+conf = sublime.load_settings('phpcsfixer.sublime-settings')
+
+def phpcsfix(view):
+    path = view.file_name()
+
+    # The command line params
+    cmd = [
+        conf.get('phpcsfixer_php_path'),
+        conf.get('phpcsfixer_phar_path'),
+        "fix",
+        path
+    ]
+
+    # Append extra options if any
+    for key, value in conf.get('phpcsfixer_options').items():
+        arg = key
+        if value != "":
+            arg += "=" + value
+        cmd.append(arg)
+
+    # Info for winOS
+    info = None
+    if os.name == 'nt':
+        info = subprocess.STARTUPINFO()
+        info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = subprocess.SW_HIDE
+
+    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, startupinfo=info)
+    if proc.stdout:
+        data = proc.communicate()[0]
+
+    return True
+
 class PhpcsfixerCommand(sublime_plugin.TextCommand):
     """A minimal PHP cs fixer plugin for sublime - by Jitendra Adhikari"""
     def run(self, edit):
-        path = self.view.file_name()
-        conf = sublime.load_settings('phpcsfixer.sublime-settings')
-
-        # The command line params
-        cmd = [
-            conf.get('phpcsfixer_php_path'), 
-            conf.get('phpcsfixer_phar_path'), 
-            "fix", 
-            path
-        ]
-
-        # Append extra options if any
-        for key, value in conf.get('phpcsfixer_options').items():
-            arg = key
-            if value != "":
-                arg += "=" + value
-            cmd.append(arg)
-
-        # Info for winOS
-        info = None
-        if os.name == 'nt':
-            info = subprocess.STARTUPINFO()
-            info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-            info.wShowWindow = subprocess.SW_HIDE
-        
-        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, startupinfo=info)
-        if proc.stdout:
-            data = proc.communicate()[0]
-
-        return True
+        return phpcsfix(self.view)
 
     def is_enabled(self):
         ext  = os.path.splitext(self.view.file_name())[1]
@@ -43,3 +47,7 @@ class PhpcsfixerCommand(sublime_plugin.TextCommand):
 
         return ext[1:] in conf.get('phpcsfixer_file_extensions')
 
+class RunFixerOnSave(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        if conf.get('phpcsfixer_fix_on_save'):
+            return phpcsfix(view)
